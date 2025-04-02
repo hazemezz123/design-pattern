3c21f3f0e6ffa0e0541d957216a278e9a8881b47
hazemezz123 <hazemezz988@gmail.com>
2025-04-02 14:22:10 +0200

    Design_Pattern

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1 style={{ textAlign: "center" }}>
+      Welcome To the deign pattern Principles
+    </h1>
+  );
+};
+
+export default App;
