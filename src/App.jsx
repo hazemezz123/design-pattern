357af856fbcd82d945e547de2d1ac65daf6382ac
hazemezz123 <hazemezz988@gmail.com>
2025-04-03 12:46:17 +0200

    docs: add WorkShop design-Pattern.pdf to documentation

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import SRP from "./designPattern/SRP";
 const App = () => {
   return (
     <div style={{ textAlign: "center" }}>
-      <h1> Welcome To the design pattern Principles</h1>
+      <h1> Welcome To the design pattern Principles the main property</h1>
       <strong>
         You are in the SRP (Single responsibility Principle) Branch Here
       </strong>
