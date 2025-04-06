6ab0e3dd6c7621ba60aa41e976fa37482cd9bfd3
hazemezz123 <hazemezz988@gmail.com>
2025-04-06 08:26:07 +0200

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import ProfileCard from "./components/ProfileCard";
 
 const App = () => {
   return (
     <h1 style={{ textAlign: "center" }}>
       Welcome To the deign pattern Principles
+      <ProfileCard name="HaZem" age="12" />
     </h1>
   );
 };
